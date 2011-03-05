bc4b73134ac45a0a19ddb496fd544b1d04e2fda6
Paul Tagell <ptagell@gmail.com>
2011-03-05 19:09:22 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tagell::Application.config.secret_token = '83c6dc54741141da459d24fe6b378b746e2b8bb6cd27f052a79ae7008b6c433d84ea1d8134abffe991345b255e7eacf4d908923f8758f0a85402ed14babd0c7e'
